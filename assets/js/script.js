4e9ae8c4eaa0930c7ca2a67b3937591c65107cc7
nezjutsu <seankral9@gmail.com>
2021-12-05 15:46:15 -0500

    added conditions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Assignment code here
 
- 
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -14,15 +14,17 @@ var upperCase = false;
 var lowerCase = true;
 var numeric = false;
 
+var numericPasswordPool = "123456789"
+var upperCasePasswordPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var lowerCasePasswordPool = "abcdefghijklmnopqrstuvwxyz";
-var specialCharacterPasswordPool = "!@#$%^&*()";
+var specialCharacterPasswordPool = "!@#$%^&*()/[]|.,`~";
 
 cardBody.innerHTML = 'this is an example.'
 console.dir(cardBody)
 
 // Write password to the #password input
 function writePassword() {
-  var finalPasswordPool = lowerCasePasswordPool;
+  var finalPasswordPool = finalPasswordPool;
   
   //var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -30,13 +32,33 @@ function writePassword() {
   while ((passwordLength < 8) || (passwordLength > 128)) {
     passwordLength = parseInt(prompt("Length of Password (Must be greater than 8 & Less than 128): ", "8"));
   }
-  specialCharacterInclusion = (confirm("Do you want special characters?: "))
+  specialCharacterInclusion = (confirm("Do you want to include special characters?"))
 
   if (specialCharacterInclusion) {
-    finalPasswordPool = finalPasswordPool + specialCharacterPasswordPool
+    finalPasswordPool += specialCharacterPasswordPool
   }
   else {finalPasswordPool += ""}
   
+  numberInclusion = (confirm("Do you want to include numbers?"))
+
+  if (numberInclusion) {
+    finalPasswordPool += numericPasswordPool
+  }
+  else {finalPasswordPool += ""}
+
+  lowerCaseInclusion = (confirm("Do you want to include lowercase letters?"))
+
+  if (lowerCaseInclusion) {
+    finalPasswordPool += lowerCasePasswordPool
+  }
+  else {finalPasswordPool += ""}
+
+  upperCaseInclusion = confirm("Would you like to include uppercase letters?")
+
+  if (upperCaseInclusion) {
+    finalPasswordPool += upperCasePasswordPool
+  }
+  else {finalPasswordPool += ""}
 
   finalPasswordPool += specialCharacterInclusion ? specialCharacterPasswordPool : "";
   console.log(finalPasswordPool);
